09457a2c9dc94d6274a1ddf8ffb6dadc12cf3ff6
MariamTarek098 <s21511204eg@std.aou.edu.eg>
2026-04-06 08:47:43 -0700

    update

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -6,16 +6,23 @@ import { cookies } from "next/headers";
 export async function decodeToken() {
   const cookieStore = await cookies();
 
-  const token =
-    cookieStore.get("__Secure-next-auth.session-token")?.value ||
-    cookieStore.get("next-auth.session-token")?.value;
+  const isProduction = process.env.NODE_ENV === "production";
+  
+  
+  const cookieName = isProduction
+    ? "__Secure-next-auth.session-token"
+    : "next-auth.session-token";
+
+  const token = cookieStore.get(cookieName)?.value;
 
   if (!token) return null;
 
+  
   const decodedToken = await decode({
     token,
     secret: process.env.NEXTAUTH_SECRET as string,
+    salt: cookieName, 
   });
 
-  return decodedToken?.routetoken;
+  return decodedToken?.routetoken as string;
 }
