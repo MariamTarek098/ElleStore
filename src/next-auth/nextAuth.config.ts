2a3fb48f88c70d34a037262e3f49f20d04f0eb1b
MariamTarek098 <s21511204eg@std.aou.edu.eg>
2026-04-06 06:52:48 -0700

    update

--- src/next-auth/nextAuth.config.ts
+++ src/next-auth/nextAuth.config.ts
@@ -1,24 +1,17 @@
-import { log } from "console";
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
-import { jwtDecode} from 'jwt-decode'
+import { jwtDecode } from "jwt-decode";
 
 export const nextAuthConfig: NextAuthOptions = {
+
+  secret: process.env.NEXTAUTH_SECRET, 
+  
   providers: [
     Credentials({
       name: "ElleStore",
       credentials: {
-        email: {
-          label: "Email",
-          placeholder: "ex:Mahmoud@gmail.com",
-          type: "email",
-        },
-        password: {
-          label: "Password",
-          placeholder: "******",
-          type: "password",
-        },
+        email: { label: "Email", placeholder: "ex:Mahmoud@gmail.com", type: "email" },
+        password: { label: "Password", placeholder: "******", type: "password" },
       },
       authorize: async function (credentials) {
         const resp = await fetch(
@@ -27,14 +20,14 @@ export const nextAuthConfig: NextAuthOptions = {
             method: "POST",
             body: JSON.stringify(credentials),
             headers: { "Content-Type": "application/json" },
-          },
+          }
         );
         const finalResult = await resp.json();
-        console.log("from auth", finalResult);
+        console.log("from auth", finalResult); 
 
         if (resp.ok) {
           const { name, email } = finalResult.user;
-          const data:{id:string} = jwtDecode(finalResult.token)
+          const data: { id: string } = jwtDecode(finalResult.token);
           return {
             name,
             email,
@@ -49,21 +42,19 @@ export const nextAuthConfig: NextAuthOptions = {
   ],
   
   callbacks: {
-jwt: function (param) {
-  if (param.user) {
-    param.token.routetoken = param.user?.tokenCredentials;
-    param.token.id = param.user.id;
-  }
-
-  return param.token;
-},
-session: function (param) {
-  if (param.session.user) {
-    param.session.user.id = param.token.id as string;
-  }
-
-  return param.session;
-}
+    jwt: async function ({ token, user }) {
+      if (user) {
+        token.routetoken = user.tokenCredentials;
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: async function ({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    }
   },
   jwt: {
     maxAge: 60 * 60 * 3,
